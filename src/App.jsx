890eec605271e1b49182ba908f690289730c8647
auritiwdesign <juancamilo.auriti@gmail.com>
2024-02-18 07:16:01 +0100

    Upload another files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import LoginSignup from "./Components/LoginSignup/LoginSignup.jsx";
+
+function App() {
+
+  return (
+    <>
+        <div className="h-screen w-screen mx-auto my-auto items-center justify-center">
+            <LoginSignup />
+        </div>
+    </>
+  )
+}
+
+export default App
